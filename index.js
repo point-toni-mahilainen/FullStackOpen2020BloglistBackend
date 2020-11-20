35dc331f45fa76e7a909e12f4ea4332aae37623c
Toni Mahilainen <toni.mahilainen@student.careeria.fi>
2020-11-20 09:49:39 +0200

    Lisäys ja haku kannasta toimii

--- index.js
+++ index.js
@@ -1,39 +1,72 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        minlength: 3,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: Number,
+        min: 0,
+        required: true
+    }
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'URI'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(() => {
-        logger.info('connected to MongoDB')
+        console.log('connected to MongoDB')
     })
     .catch((error) => {
-        logger.error('error connection to MongoDB:', error.message)
+        console.log('error connection to MongoDB:', error.message)
     })
 
 app.use(cors())
 app.use(express.json())
+app.use(requestLogger)
 
-app.get('/api/blogs', (request, response) => {
+app.get('/api/blogs', (request, response, next) => {
     Blog
         .find({})
         .then(blogs => {
             response.json(blogs)
         })
+        .catch(error => next(error))
 })
 
-app.post('/api/blogs', (request, response) => {
+app.post('/api/blogs', (request, response, next) => {
     const blog = new Blog(request.body)
 
     blog
@@ -41,9 +74,29 @@ app.post('/api/blogs', (request, response) => {
         .then(result => {
             response.status(201).json(result)
         })
+        .catch(error => next(error))
 })
 
-const PORT = 3003
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'Unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'Unknown endpoint' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).send(error.message)
+    }
+
+    next(error)
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
