d6f4fbd83786e76bffb2f587644566b945fd99fb
Toni Mahilainen <toni.mahilainen@student.careeria.fi>
2020-11-25 18:55:56 +0200

    Tehtävät 4.18 - 4.20 tehty

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(() => {
@@ -20,7 +21,9 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,16 +2,29 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const logger = require('../utils/logger')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
+
 blogsRouter.post('/', async (request, response) => {
     let body = request.body
 
-    const users = await User.find({})
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     if (!body.likes) {
         body = {
@@ -21,16 +34,17 @@ blogsRouter.post('/', async (request, response) => {
     }
 
     let blog = new Blog({
-        title: "The blog 7",
-        author: "Kalle Mäkilä",
-        user: users[0],
-        url: "https://blog.theblog7.com",
-        likes: 20000
+        title: body.title,
+        author: body.author,
+        user: user._id,
+        url: body.url,
+        likes: body.likes
     })
 
     const savedBlog = await blog.save()
-    users[0].blogs = users[0].blogs.concat(savedBlog._id)
-    await users[0].save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+
+    await user.save()
     response.status(201).json(savedBlog)
 })
 
